b157c559a9bffde87619af2282c31a3374d1ac2b
Harun <122699217+ornamynt-admin@users.noreply.github.com>
2024-03-21 15:48:41 +0000

    Added the nextAuth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Providers } from "./providers";
 import { Navbar } from "@/components/navbar";
-import { Link } from "@nextui-org/link";
+import AuthProvider from "@/components/providers";
 import clsx from "clsx";
 import { SiteFooter } from "@/components/site-footer";
 
@@ -54,7 +54,9 @@ export default async function RootLayout({
 						<Navbar />
 						
 						<main className="w-full mx-auto pt-16">
-							{children}
+							<AuthProvider>
+								{children}
+							</AuthProvider>
 						</main>
 						
 						<SiteFooter/>
