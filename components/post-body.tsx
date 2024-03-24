ea61a727eeb8d4be14212af4f52b48f487d4a4c0
Harun <122699217+ornamynt-admin@users.noreply.github.com>
2024-03-24 10:46:15 +0000

    Updated the text to white

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -30,7 +30,7 @@ const myPortableTextComponents: Partial<PortableTextReactComponents> = {
 
 export default function PostBody({ content }: {content: any}) {
   return (
-    <div className="mx-auto max-w-5xl text-lg leading-relaxed prose">
+    <div className="mx-auto max-w-5xl text-lg leading-relaxed prose text-white">
       <PortableText value={content} components={myPortableTextComponents} />
     </div>
   )
