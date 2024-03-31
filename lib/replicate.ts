e8fb8a21f30506416d6006bf2b139439e751e959
Harun <122699217+ornamynt-admin@users.noreply.github.com>
2024-03-31 23:36:54 +0100

    Removed token and increased duration

--- lib/replicate.ts
+++ lib/replicate.ts
@@ -1,8 +1,5 @@
 import Replicate from "replicate";
 
-console.log("REPLICATE KEY")
-console.log(process.env.REPLICATE_API_TOKEN)
-
 export const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!,
 });
